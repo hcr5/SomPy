1d894182e4a7ce4013a3592c36c46148f29d5e5f
hcr5 <157815647+hcr5@users.noreply.github.com>
2024-07-13 06:35:46 +0200

    Update jeugdjournaal.py

--- jeugdjournaal/jeugdjournaal.py
+++ jeugdjournaal/jeugdjournaal.py
@@ -2,6 +2,62 @@ import requests as _requests
 from bs4 import BeautifulSoup as _BeautifulSoup
 import json as _json
 
+class Article:
+    def __init__(self, title, content, images):
+        self.title = title
+        self.content = content
+        self.images = images
+
+class PollIds:
+    def __init__(self, id_1, id_2):
+        self.id_1 = id_1
+        self.id_2 = id_2
+
+class PollResults:
+    def __init__(self, total_votes, answers):
+        self.total_votes = total_votes
+        self.answers = answers
+
+class Comment:
+    def __init__(self, id, content, name, pinned, published_at):
+        self.id = id
+        self.content = content
+        self.name = name
+        self.pinned = pinned
+        self.published_at = published_at
+
+class Reaction:
+    def __init__(self, id, content, name, pinned, published_at):
+        self.id = id
+        self.content = content
+        self.name = name
+        self.pinned = pinned
+        self.published_at = published_at
+
+class Item:
+    def __init__(self, title, id):
+        self.title = title
+        self.id = id
+
+def read_item(item_id):
+    try:
+        r = _requests.get(f"https://jeugdjournaal.nl/artikel/{item_id}")
+        r.raise_for_status()
+    except _requests.exceptions.RequestException as e:
+        raise Exception(f"Error reading article {item_id}: {e}")
+    
+    soup = _BeautifulSoup(r.content, 'html.parser')
+
+    title_element = soup.find(class_='sc-799cb68a-0 sc-f0a7871b-6 frnEMw fmrOZD')
+    title = title_element.get_text(strip=True) if title_element else 'No title found'
+
+    content_elements = soup.find_all(class_='sc-5ad4567c-0 bAROSX')
+    content = '\n\n'.join(element.get_text(strip=True) for element in content_elements)
+
+    image_urls = [img.get('src') for img in soup.find_all('img', src=True)]
+
+    return Article(title, content, image_urls)
+
 def get_poll_ids(item_id):
     try:
         response = _requests.get(f"https://jeugdjournaal.nl/artikel/{item_id}")
@@ -21,47 +77,30 @@ def get_poll_ids(item_id):
         raise Exception(f"Error parsing JSON data: {e}")
     
     content = data.get('props', {}).get('pageProps', {}).get('data', {}).get('content', [])
-    hash_1 = None
-    hash_2 = None
-    hash_1_name = None
-    hash_2_name = None
+    id_1 = id_2 = id_1_name = id_2_name = None
     
     for item in content:
         if item.get('type') == 'poll':
             poll_content = item.get('content', {})
             answers = poll_content.get('answers', [])
-            
             if len(answers) >= 2:
-                hash_1 = answers[0].get('hash')
-                hash_2 = answers[1].get('hash')
-                hash_1_name = answers[0].get('text')
-                hash_2_name = answers[1].get('text')
+                id_1 = answers[0].get('hash')
+                id_2 = answers[1].get('hash')
+                id_1_name = answers[0].get('text')
+                id_2_name = answers[1].get('text')
                 break
     
-    if not hash_1 or not hash_2:
+    if not id_1 or not id_2:
         raise Exception(f"No poll IDs found for {item_id}, maybe it doesn't contain any polls?")
-        
-    return {
-        "id_1": {
-            "id": hash_1,
-            "text": hash_1_name
-        },
-        "id_2": {
-            "id": hash_2,
-            "text": hash_2_name
-        }
-    }
-
+    
+    return PollIds({"id": id_1, "text": id_1_name}, {"id": id_2, "text": id_2_name})
 
 def vote_in_poll(vote_hash):
     try:
         url = f'https://jeugdjournaal.nl/api/poll/vote/{vote_hash}'
-        response = _requests.post(url)
-        response.raise_for_status()
+        _requests.post(url)
     except _requests.exceptions.RequestException as e:
         raise Exception(f"Error voting in poll {vote_hash}: {e}")
-    
-    return response.json()
 
 def get_poll_data(item_id):
     try:
@@ -85,8 +124,9 @@ def get_poll_data(item_id):
     
     for item in data["props"]["pageProps"]["data"]["content"]:
         if item["type"] == "poll":
-            poll_results["totalVotes"] = item["content"]["noOfVotes"]
-            poll_results["answers"] = [{"text": answer["text"], "votes": answer["votes"]} for answer in item["content"]["answers"]]
+            total_votes = item["content"]["noOfVotes"]
+            answers = [{"text": answer["text"], "votes": answer["votes"]} for answer in item["content"]["answers"]]
+            poll_results = PollResults(total_votes, answers)
             break
     
     return poll_results
@@ -99,22 +139,9 @@ def get_comments(item_id, limit):
         raise Exception(f"Error fetching comments for item {item_id}: {e}")
     
     json_response = response.json()
-    formatted_comments = []
+    comments = [Comment(item['id'], item['text'], item['name'], item['pinned'], item['publishedAt']) for item in json_response['items']]
 
-    for item in json_response['items']:
-        formatted_comment = {
-            "id": item['id'],
-            "content": item['text'],
-            "name": item['name'],
-            "pinned": item['pinned'],
-            "publishedAt": item['publishedAt']
-        }
-        formatted_comments.append(formatted_comment)
-
-    return {
-        "comments": formatted_comments
-    }
-
+    return comments
 
 def get_comment_reactions(item_id, comment_id, limit):
     try:
@@ -124,22 +151,9 @@ def get_comment_reactions(item_id, comment_id, limit):
         raise Exception(f"Error fetching comment reactions for item {item_id}, comment {comment_id}: {e}")
     
     json_response = response.json()
-    formatted_reactions = []
+    reactions = [Reaction(item['id'], item['text'], item['name'], item['pinned'], item['publishedAt']) for item in json_response['items']]
 
-    for item in json_response['items']:
-        formatted_reaction = {
-            "id": item['id'],
-            "content": item['text'],
-            "name": item['name'],
-            "pinned": item['pinned'],
-            "publishedAt": item['publishedAt']
-        }
-        formatted_reactions.append(formatted_reaction)
-
-    return {
-        "reactions": formatted_reactions
-    }
-
+    return reactions
 
 def post_comment(item_id, name, content):
     payload = {
@@ -152,12 +166,6 @@ def post_comment(item_id, name, content):
     except _requests.exceptions.RequestException as e:
         raise Exception(f"Error posting comment for item {item_id}: {e}")
 
-    return {
-        "id": r.json()["id"],
-        "publishedAt": r.json()["publishedAt"]
-    }
-
-
 def get_items():
     try:
         r = _requests.get("https://jeugdjournaal.nl/")
@@ -168,38 +176,6 @@ def get_items():
     soup = _BeautifulSoup(r.content, 'html.parser')
 
     elements = soup.find_all(class_='sc-a2203b4a-5 ixVSEc')
-    items = []
+    items = [Item(element.get_text(strip=True), element.find_parent('a')['href'].split('/')[-1].split('-')[0]) for element in elements]
 
-    for element in elements:
-        title = element.get_text(strip=True)
-        href = element.find_parent('a')['href']
-        id = href.split('/')[-1].split('-')[0]
-        items.append({"title": title, "id": id})
-
-    return {
-        "items": items
-    }
-
-
-def read_item(item_id):
-    try:
-        r = _requests.get(f"https://jeugdjournaal.nl/artikel/{item_id}")
-        r.raise_for_status()
-    except _requests.exceptions.RequestException as e:
-        raise Exception(f"Error reading article {item_id}: {e}")
-    
-    soup = _BeautifulSoup(r.content, 'html.parser')
-
-    title_element = soup.find(class_='sc-799cb68a-0 sc-f0a7871b-6 frnEMw fmrOZD')
-    title = title_element.get_text(strip=True) if title_element else 'No title found'
-
-    content_elements = soup.find_all(class_='sc-5ad4567c-0 bAROSX')
-    content = '\n\n'.join(element.get_text(strip=True) for element in content_elements)
-
-    image_urls = [img.get('src') for img in soup.find_all('img', src=True)]
-
-    return {
-        "title": title,
-        "content": content,
-        "image_urls": image_urls
-    }
+    return items
